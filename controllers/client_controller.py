450f8a09e0a5dcb9245089a6387043a4afa84659
NickSpringall <nick@fnqpianos.com.au>
2023-07-23 23:20:46 +1000

    create client and technician new user route

--- controllers/client_controller.py
+++ controllers/client_controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
-from init import db
+from flask import Blueprint, request
+from init import db, bcrypt
 from models.client import Client, clients_schema, client_schema
 from models.technician import Technician
 
@@ -11,3 +11,19 @@ def get_all_clients():
     stmt = db.select(Client).order_by(Client.name)
     clients = db.session.scalars(stmt)
     return clients_schema.dump(clients)
+
+@client_bp.route ('/', methods = ['POST'])
+def create_client():
+    body_data = request.get_json()
+    client = Client(
+        name = body_data.get('name'),
+        address = body_data.get('address'),
+        phone = body_data.get('phone'),
+        email = body_data.get('email'),
+        password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8'),
+        technician_id = body_data.get('technician_id')
+    )
+    db.session.add(client)
+    db.session.commit()
+
+    return client_schema.dump(client), 201
--- controllers/technician_controller.py
+++ controllers/technician_controller.py
@@ -1,16 +1,32 @@
-from flask import Blueprint
-from init import db
+from flask import Blueprint, request
+from init import db, bcrypt
 from models.technician import Technician, technician_schema, technicians_schema
 from models.client import Client, clients_schema, client_schema
 
 technician_bp = Blueprint('technicians', __name__, url_prefix='/technicians')
-
 
 @technician_bp.route ('/')
 def get_all_technicians():
     stmt = db.select(Technician).order_by(Technician.first_name)
     technicians = db.session.scalars(stmt)
     return technicians_schema.dump(technicians)
+
+
+@technician_bp.route ('/', methods = ['POST'])
+def create_technician():
+    body_data = request.get_json()
+    technician = Technician(
+        first_name=body_data.get('first_name'),
+        last_name=body_data.get('last_name'),
+        address=body_data.get('address'),
+        phone=body_data.get('phone'),
+        email=body_data.get('email'),
+        password=bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
+    )
+    db.session.add(technician)
+    db.session.commit()
+
+    return technician_schema.dump(technician), 201
 
 
 @technician_bp.route ('/<int:id>/clients')
