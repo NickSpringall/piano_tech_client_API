53f43ae0f3c62c6f62fe6f4c949e368793b6f89f
NickSpringall <nick@fnqpianos.com.au>
2023-07-26 19:35:17 +1000

    fix technician auth decorator

--- controllers/client_controller.py
+++ controllers/client_controller.py
@@ -16,33 +16,41 @@ def get_all_clients():
     return clients_schema.dump(clients)
 
 
-def check_if_technician(user_id):
-    if 'technician' in user_id:
-        return 'user is technician'
-    else: 
-        return
+# def check_if_technician(user_id):
+#     if 'technician' in user_id:
+#         return 'user is technician'
+#     else: 
+#         return
 
-
+def check_if_technician(fn):
+    @functools.wraps(fn)
+    def wrapper():
+        user = get_jwt_identity()
+        if 'technician' in user:
+            return (fn())
+        else:
+            return {"error": "unauthorised user"}, 401
+    return wrapper
+        
 @client_bp.route ('/', methods = ['POST'])
 @jwt_required()
+@check_if_technician
 def create_client():
     user = get_jwt_identity()
-    if check_if_technician(user):
-        body_data = request.get_json()
-        client = Client(
-            name = body_data.get('name'),
-            address = body_data.get('address'),
-            phone = body_data.get('phone'),
-            email = body_data.get('email'),
-            password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8'),
-            technician_id = body_data.get('technician_id')
-        )
-        db.session.add(client)
-        db.session.commit()
 
-        return client_schema.dump(client), 201
-    else:
-        return {"error": "unauthorised user"}, 401
+    body_data = request.get_json()
+    client = Client(
+        name = body_data.get('name'),
+        address = body_data.get('address'),
+        phone = body_data.get('phone'),
+        email = body_data.get('email'),
+        password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8'),
+        technician_id = body_data.get('technician_id')
+    )
+    db.session.add(client)
+    db.session.commit()
+
+    return client_schema.dump(client), 201
+
+
     
-
-        
